36884c725af1e2f251b7b4a535eb67c18f48df2a
Doug Harple <dharple@gmail.com>
2020-01-14 23:49:37 -0500

    Continue modernizing code

--- public-old/include/tables.sql
+++ public-old/include/tables.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS section;
 CREATE TABLE section (
   id int(11) NOT NULL auto_increment,
   name varchar(255) NOT NULL default '',
@@ -10,13 +11,14 @@ CREATE TABLE section (
 ALTER TABLE section ADD INDEX primary_display_idx (user_id, status, name);
 
 
+DROP TABLE IF EXISTS item;
 CREATE TABLE item (
   id int(11) NOT NULL auto_increment,
   section_id int(11) NOT NULL default '0',
   task varchar(255) NOT NULL default '',
   status enum('Open','Closed','Deleted') NOT NULL default 'Open',
-  created datetime NOT NULL default '0000-00-00 00:00:00',
-  completed datetime NOT NULL default '0000-00-00 00:00:00',
+  created datetime NULL,
+  completed datetime NULL,
   priority TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
   estimate decimal(5,1) NOT NULL default '0.0',
   user_id tinyint(3) unsigned NOT NULL default '0',
@@ -29,19 +31,21 @@ CREATE TABLE item (
 
 ALTER TABLE item ADD index primary_count_idx (user_id, status, completed);
 
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id tinyint(3) unsigned NOT NULL auto_increment,
   username varchar(32) NOT NULL default '',
   fullname tinytext NOT NULL,
   password tinytext NOT NULL,
   timezone VARCHAR(32) NOT NULL DEFAULT 'US/Eastern',
-  display_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL,
-  print_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL,
-  export_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL,
+  display_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL default '0',
+  print_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL default '0',
+  export_stylesheet_id SMALLINT(5) UNSIGNED NOT NULL default '0',
   PRIMARY KEY  (id),
   UNIQUE KEY username (username)
 );
 
+DROP TABLE IF EXISTS session;
 CREATE TABLE session (
   id int(10) unsigned NOT NULL auto_increment,
   session_id VARCHAR(255) NOT NULL,
@@ -53,6 +57,7 @@ CREATE TABLE session (
 );
 
 
+DROP TABLE IF EXISTS user_stylesheet;
 CREATE TABLE user_stylesheet (
   id SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id TINYINT(3) UNSIGNED NOT NULL,
@@ -65,6 +70,7 @@ CREATE TABLE user_stylesheet (
 );
 
 
+DROP TABLE IF EXISTS recurring_item;
 CREATE TABLE recurring_item (
   id INT(11) NOT NULL AUTO_INCREMENT,
   user_id TINYINT(3) UNSIGNED NOT NULL default '0',
