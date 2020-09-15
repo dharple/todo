c57d1859efed90e31b2b404b63a5fb84b0f5a61d
Doug Harple <dharple@gmail.com>
2020-09-14 21:37:03 -0400

    Render bulk of index page via twig; add site base page

--- public/index.php
+++ public/index.php
@@ -112,195 +112,27 @@ $listDisplay->setFooter($twig->render('partials/index/summary.php.twig', [
 
 $user_id = $_SESSION['user_id'];
 
-$twig->display('partials/page/header.html.twig', [
-    'title' => 'To Do List For ' . date('F jS, Y')
-]);
-
-?>
-
-<div class="noprint">
-<table width=100%>
-<tr>
-<td align=left valign=top>
-<b>To Do List For <?php print($user->getFullname()); ?></b>
-<br>
-
-Items Done:<br>
-&nbsp;&nbsp; Today: <a href="show_done.php?view=today">
-<?php
-print($itemStats->doneToday());
-?></a>,
-Yesterday: <a href="show_done.php?view=yesterday">
-<?php
-print($itemStats->doneYesterday());
-?></a>
-<br>
-
-&nbsp;&nbsp; This Week: <a href="show_done.php?view=week">
-<?php
-print($itemStats->doneThisWeek());
-?></a>,
-Last Week: <a href="show_done.php?view=lastweek">
-<?php
-print($itemStats->doneLastWeek());
-?></a>
-<br>
-
-&nbsp;&nbsp; This Month: <a href="show_done.php?view=month">
-<?php
-print($itemStats->doneThisMonth());
-?></a>,
-Last Month: <a href="show_done.php?view=lastmonth">
-<?php
-print($itemStats->doneLastMonth());
-?></a>
-<br>
-
-&nbsp;&nbsp; 3 / 6 / 9 / 12 Months: <a href="show_done.php?view=month3">
-<?php
-print($itemStats->donePreviousMonths(3));
-?></a> /
-<a href="show_done.php?view=month6">
-<?php
-print($itemStats->donePreviousMonths(6));
-?></a> /
-<a href="show_done.php?view=month9">
-<?php
-print($itemStats->donePreviousMonths(9));
-?></a> /
-<a href="show_done.php?view=month12">
-<?php
-print($itemStats->donePreviousMonths(12));
-?></a>
-<br>
-
-Items Done Since Start: <a href="show_done.php">
-<?php
-print($itemStats->doneTotal());
-?></a>
-<br>
-
-Average Turnaround: <?= number_format($avg, 1) ?> days
-<br>
-</td>
-<td align=right valign=top>
-Filter Closed Items:&nbsp;&nbsp;
-<?php
-
-
-foreach ($closed_display as $value => $display) {
-    print(' ');
-    if ($display_filter_closed == $value) {
-        print($display);
-    } else {
-        print('<a href="index.php?filter_closed=' . $value . '">' . $display . '</a>');
-    }
-}
-
-?>
-<br>
-
-Filter Priority:&nbsp;&nbsp;
-<?php
-
-foreach ($priority_display as $value => $display) {
-    print(' ');
-    if ($display_filter_priority == $value) {
-        print($display);
-    } else {
-        print('<a href="index.php?filter_priority=' . $value . '">' . $display . '</a>');
-    }
-}
-
-?>
-<br>
-
-Show Priority:&nbsp;&nbsp;
-<?php
-
-foreach ($show_priority_display as $value => $display) {
-    print(' ');
-    if ($display_show_priority == $value) {
-        print($display);
-    } else {
-        print('<a href="index.php?show_priority=' . $value . '">' . $display . '</a>');
-    }
-}
-
-?>
-<br>
-
-Filter Aging:&nbsp;&nbsp;
-<?php
-
-foreach ($aging_display as $value => $display) {
-    print(' ');
-    if ($display_filter_aging == $value) {
-        print($display);
-    } else {
-        print('<a href="index.php?filter_aging=' . $value . '">' . $display . '</a>');
-    }
-}
-
-?>
- days old
-<br>
-
-Show Inactive Sections:&nbsp;&nbsp;
-<?php
-
-if ($display_show_inactive == 'y') {
-    print('Yes <a href="index.php?show_inactive=n">No</a>');
-} else {
-    print('<a href="index.php?show_inactive=y">Yes</a> No');
-}
-
-?>
-<br>
-Display Settings: <a href="index.php?reset_display_settings=1">Reset</a>
-
-</td>
-</tr>
-</table>
-
-<hr>
-
-</div>
-
-<div class="print">
-<p align=center><b><?php print(date('F jS, Y')); ?></b> - <b><?php print($user->getFullname()); ?></b></p>
-</div>
-
-<form method=POST>
-
-<?php
-
-print($listDisplay->getOutput());
-
+$listOutput = $listDisplay->getOutput();
 $itemCount = $listDisplay->getOutputCount();
 
 $sectionList = new SimpleList($db, Section::class);
 $sectionCount = $sectionList->count("WHERE user_id = '" . addslashes($user_id) . "' AND status = 'Active'");
 
-?>
-
-<div class="noprint">
-<hr>
-<br>
-
-<?php
-
-$twig->display('partials/index/footer.html.twig', [
-    'hasItems'      => ($itemCount > 0),
-    'hasSections'   => ($sectionCount > 0),
-    'showDuplicate' => ($display_filter_closed != 'none'),
+$twig->display('index.html.twig', [
+    'avg'                    => $avg,
+    'filterAgingSelected'    => $display_filter_aging,
+    'filterAgingValues'      => $aging_display,
+    'filterClosedSelected'   => $display_filter_closed,
+    'filterClosedValues'     => $closed_display,
+    'filterPrioritySelected' => $display_filter_priority,
+    'filterPriorityValues'   => $priority_display,
+    'hasItems'               => ($itemCount > 0),
+    'hasSections'            => ($sectionCount > 0),
+    'itemStats'              => $itemStats, 
+    'list'                   => $listOutput,
+    'showDuplicate'          => ($display_filter_closed != 'none'),
+    'showInactiveSelected'   => $display_show_inactive,
+    'showPrioritySelected'   => $display_show_priority,
+    'showPriorityValues'     => $show_priority_display,
+    'user'                   => $user,
 ]);
-
-?>
-</div>
-
-</form>
-
-<?php
-
-$twig->display('partials/page/footer.html.twig');
