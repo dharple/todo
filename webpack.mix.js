d34defe58fcadf49431ac5bb24dbf828a7b687d7
Doug Harple <dharple@gmail.com>
2021-02-05 22:03:34 -0500

    Update external assets to be loaded via node/npm (resolves #4)

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .combine([
+        'node_modules/jquery/dist/jquery.slim.js',
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+        'node_modules/chart.js/dist/Chart.js',
+        'node_modules/moment/moment.js',
+    ], 'public/js/vendor.js')
+    .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/')
+    .copy('node_modules/bootstrap/dist/css/bootstrap.min.css.map', 'public/css/');
