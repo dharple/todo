17381579995621b217e1c5a02721e95fa91a7c84
Doug Harple <dharple@gmail.com>
2021-02-07 01:41:07 -0500

    Update doctrine cli-config.php

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,13 +1,24 @@
 <?php
 
-/**
- * @noinspection PhpUnhandledExceptionInspection
- */
-
+use App\Auth\Guard;
 use App\Helper;
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-require_once "vendor/autoload.php";
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
 
 $entityManager = Helper::getEntityManager();
 
