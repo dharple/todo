400b60fd491b91f7eaed4ca4668cef28aa0cfe26
Doug Harple <dharple@gmail.com>
2026-04-25 12:50:01 +0000

    Update rector to v1.0.0

--- rector.php
+++ rector.php
@@ -9,7 +9,6 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/config',
-        __DIR__ . '/include',
         __DIR__ . '/public',
         __DIR__ . '/src',
         __DIR__ . '/tests',
