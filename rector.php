7618fc09a1f7270d23a65307eb1ee28364389886
Doug Harple <dharple@gmail.com>
2026-05-24 11:14:27 -0400

    Update rector config, ignore Override attribute

--- rector.php
+++ rector.php
@@ -2,20 +2,15 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
         __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php83: true)
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class
     ]);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_83,
-    ]);
-};
